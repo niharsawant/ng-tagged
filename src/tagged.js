0ce79e9c304903fa9745395bc60fa3b5bd753bc3
Nihar Sawant <nihar.sawant@gmail.com>
2014-05-23 23:17:38 +0530

    feat: base directory setup completed

--- src/tagged.js
+++ src/tagged.js
@@ -1,5 +1,5 @@
 (function () {
-  
+
   'use strict';
 
   var module = angular.module('ngTagged', []);
@@ -8,6 +8,7 @@
     return {
       restrict : 'E',
       replace : true,
+      templateUrl : '/ng-tagged/templates/tagged.html',
       transclude : true,
       link : function (scope, element, attrs) {
         console.log(scope);
